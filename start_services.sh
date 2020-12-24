3fff651e69205806ce299c9a0268e7fe8454f3c3
profbrunocoutinho <bccout@ifes.edu.br>
2020-12-24 15:49:25 -0300

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -2,6 +2,5 @@ cd app/
 
 # Start rasa server with nlu model
 
-rasa run --model models --enable-api --cors "*" --debug \
-         -p $PORT
+rasa run --model models --enable-api --cors "*" --debug -p $PORT
          
